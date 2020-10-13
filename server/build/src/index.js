cf40efa86cdad00e7ab7448d9668b0c0a2d2101c
mikebutts <mgbutts@gmail.com>
2020-10-13 07:52:18 -0400

    added apollo server

---
+++ server/build/src/index.js
@@ -0,0 +1,12 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = express_1.default();
+const port = 9000;
+app.get("/", (req, res) => {
+    res.send(`hello world r im running  e on ${port}`);
+});
+app.listen(port);
---
+++ server/src/index.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+
+const app = express();
+const port = 9000;
+
+const server = new ApolloServer();
+server.applyMiddleware({ app, path: "/api" });
+
+app.listen(port);
