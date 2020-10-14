46a967edd977d8251856a806a3c2357f065f7dc6
mikebutts <mgbutts@gmail.com>
2020-10-14 13:31:39 -0400

    setup graphql for listing

--- server/src/graphql.ts
+++ server/src/graphql.ts
@@ -1,1 +1,48 @@
-import { GraphQLObjectType, GraphQLSchema } from "graphql";
+import {
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLSchema,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLList,
+  GraphQLNonNull,
+} from "graphql";
+
+const Listing = new GraphQLObjectType({
+  name: "Listing",
+  fields: {
+    id: { type: GraphQLNonNull(GraphQLID) },
+    title: { type: GraphQLNonNull(GraphQLString) },
+    image: { type: GraphQLNonNull(GraphQLString) },
+    address: { type: GraphQLNonNull(GraphQLString) },
+    price: { type: GraphQLNonNull(GraphQLInt) },
+    numOfGuests: { type: GraphQLNonNull(GraphQLInt) },
+    numOfBeds: { type: GraphQLNonNull(GraphQLInt) },
+    numOfBaths: { type: GraphQLNonNull(GraphQLInt) },
+    rating: { type: GraphQLNonNull(GraphQLInt) },
+  },
+});
+
+const query = new GraphQLObjectType({
+  name: "Query",
+  fields: {
+    listings: {
+      type: GraphQLNonNull(GraphQLList(GraphQLNonNull(Listing))),
+      resolve: () => {
+        return listings;
+      },
+    },
+  },
+});
+
+const mutation = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    hello: {
+      type: GraphQLString,
+      resolve: () => "hello world",
+    },
+  },
+});
+
+export const schema = new GraphQLSchema({ query, mutation });
